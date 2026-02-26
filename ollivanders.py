ac38fef7ee06fa50e67d732b14e0f535fcaa2f3e
Juan071825 <juanmateoalvarez@gmail.com>
2026-02-26 10:15:02 +0100

    feat(ollivanders.py): :sparkles: Añadir clases restantes.

--- ollivanders.py
+++ ollivanders.py
@@ -3,11 +3,11 @@ class Ollivanders():
     def __init__(self,items):
         self.items = items
 
-    def update_inventory_quality(self):
+    def update_inventory(self):
         for item in self.items:
             item.update_quality()
 
-    def get_items(self):
+    def get_inventory(self):
         return self.items
 
 
@@ -18,42 +18,106 @@ class Item:
         self.quality = quality
 
     def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
-
+        return f"{self.name}, {self.sell_in}, {self.quality}"
     
+
+#class Interfaz():
+#   def update_quality(self):
+#       pass
+
+
 class NormalItem(Item):
     def __init__(self, name, sell_in, quality):
         super().__init__(name, sell_in, quality)
+
+    def setSell_in(self):
+        self.sell_in -= 1
+
+    def setQuality(self, valor):
+        if self.quality + valor in range(0,51):
+            self.quality += valor
+        elif self.quality + valor > 50:
+            self.quality = 50
+        else:
+            self.quality = 0
+
+
+    def update_quality(self):
+        if self.sell_in >= 0:
+            self.setQuality(-1)
+        else:
+            self.setQuality(-2)
+        self.setSell_in()
+        
+    
+class ConjuredItem(NormalItem):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        if self.sell_in >= 0:
+            self.setQuality(-2)
+        else:
+            self.setQuality(-4)
+        self.setSell_in()
+
+
+class AgedBrie(NormalItem):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        if self.sell_in > 0:
+            self.setQuality(1)
+        else:
+            self.setQuality(2)
+        self.setSell_in()
+
+    
+class Sulfuras(NormalItem):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        self.quality = 80
+        self.setSell_in()
+
+
+
+class Backstage(NormalItem):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        if self.sell_in > 10:
+            self.setQuality(1)
+        
+        elif self.sell_in > 5:
+            self.setQuality(2)
+
+        elif self.sell_in > 0:
+            self.setQuality(3)
+
+        else:
+            self.quality = 0
+        self.setSell_in
+
+
         
 
-    def update_quality(self):
-        self.sell_in -= 1
-        if self.sell_in >= 0:
-            self.quality -= 1
-        else:
-            self.quality -= 2
-        return [self.name, self.sell_in, self.quality]
 
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
-
-
-class Interfaz():
-
-    def update_quality(self):
-        pass
 
 
 
 if __name__ == '__main__':
 
-    def test_Ollivanders_get_items():
-        inventory = Ollivanders(['anillo', 'mazo', 'copa'])
-        assert inventory.get_items() == ['anillo', 'mazo', 'copa']
-        assert inventory.get_items() != ['anillo', 'maza', 'copa']        
-        print("Ollivanders().get_items funciona")
+    #def test_Ollivanders_get_items():
+    #    inventory = Ollivanders(['anillo', 'mazo', 'copa'])
+    #    assert inventory.get_inventory() == ['anillo', 'mazo', 'copa']
+    #    assert inventory.get_inventory() != ['anillo', 'maza', 'copa']        
+    #    print("Ollivanders().get_items funciona")
 
-    test_Ollivanders_get_items()
+    #test_Ollivanders_get_items()
 
 
     def test_NormalItem_update_quality():
